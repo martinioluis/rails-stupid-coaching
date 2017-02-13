91f2a01498e6f3f83e0b1082df80f52897adbfea
martin peubey <martin.peubey@gmail.com>
2017-02-13 14:16:17 +0100

    ok

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,7 +1,10 @@
 class CoachingController < ApplicationController
   def answer
+    @question = params[:question]
+    @answer = ["plop", "plop2"].sample
   end
 
   def ask
+    @question = params[:question]
   end
 end
